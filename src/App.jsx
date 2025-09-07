6f7d795c5ea1547a13b7c07aca97f44b3aa1689d
Aditya Chaudhary <ac161050@gmail.com>
2025-09-07 11:25:22 +0530

    Beginning

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function App() {
+  return (
+    <main> 
+      <h1 className="text-5xl text-orange-500 font-bold">Welcome to Award Winning Website</h1>
+    </main>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        zentry: ['zentry', 'sanf-serif'],
+        general: ['general', 'sanf-serif'],
+        'circular-web': ['circular-web', 'sanf-serif'],
+        'robert-regular': ['robert-regular', 'sanf-serif'],
+        'robert-medium': ['robert-medium', 'sanf-serif'],
+      }
+    }
+  },
+  plugins: [],
+}
