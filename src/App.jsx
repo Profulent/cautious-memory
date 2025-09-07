f54324372aa840ce0d006fa1568c851e916090d2
Aditya1149 <hydrogenspectrum12345@gmail.com>
2025-09-07 20:03:34 +0530

    new tailwind version property

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react"
 function App() {
   return (
     <main> 
-      <h1 className="text-5xl text-red-400 font-bold font-robert-regular">Welcome to Award Winning Website</h1>
+      <h1 className="text-5xl text-red-700 font-bold font-circular-web">Welcome to Award Winning Website</h1>
     </main>
   )
 }
